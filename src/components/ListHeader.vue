b2080e83250317233103676da7f2dd942ce5bb59
kravetsiv <kravetsiv@modeltech.ru>
2024-10-02 14:48:06 +0500

    В list-header добавлены отступы

--- src/components/ListHeader.vue
+++ src/components/ListHeader.vue
@@ -1,24 +1,27 @@
 <script setup>
-import { useRouter } from 'vue-router';
+import { useRouter } from "vue-router";
 
-defineProps(["caption"])
+defineProps(["caption"]);
 const router = useRouter();
 
 const handleBack = () => {
   router.back();
-}
+};
 </script>
 
 <template>
   <div class="q-mb-lg">
     <q-toolbar>
       <q-toolbar-title>{{ caption }}</q-toolbar-title>
+      <div class="q-gutter-sm q-mx-sm">
+        <slot name="buttons" />
+      </div>
+
       <q-input outlined dense label="Быстрый поиск">
         <template v-slot:append>
           <q-icon name="search" />
         </template>
       </q-input>
-      <slot name="buttons" />
     </q-toolbar>
     <q-separator />
   </div>
